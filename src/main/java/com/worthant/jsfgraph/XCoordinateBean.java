fbd3e6b85faeda577a2452f1134cc70dbbab75ae
worthant <b_dvorkin@niuitmo.ru>
2023-11-24 23:46:56 +0300

    :hammer: Apply Lombok, write javadoc

--- src/main/java/com/worthant/jsfgraph/XCoordinateBean.java
+++ src/main/java/com/worthant/jsfgraph/XCoordinateBean.java
@@ -2,6 +2,8 @@ package com.worthant.jsfgraph;
 
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.validator.ValidatorException;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Objects;
@@ -10,41 +12,15 @@ import java.util.Objects;
  * Managed bean for handling X coordinate value in JSF application.
  * Provides functionality to get and set X value, and to validate it.
  */
+@Data
+@NoArgsConstructor
 public class XCoordinateBean implements Serializable {
     private Double x = 0.0;
-
-    public Double getXBeanValue(){
-        return x;
-    }
-
-    public void setXBeanValue(Double x) {
-        this.x = x;
-    }
 
     public void validateXBeanValue(Object o){
         if (o == null){
             FacesMessage message = new FacesMessage("X value should be in (-5, 5) interval");
             throw new ValidatorException(message);
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        XCoordinateBean xCoordinateBean = (XCoordinateBean) o;
-        return Objects.equals(x, xCoordinateBean.x);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x);
-    }
-
-    @Override
-    public String toString() {
-        return "XCoordinateBean{" +
-                "x=" + x +
-                '}';
     }
 }
--- src/main/java/com/worthant/jsfgraph/YCoordinateBean.java
+++ src/main/java/com/worthant/jsfgraph/YCoordinateBean.java
@@ -2,6 +2,8 @@ package com.worthant.jsfgraph;
 
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.validator.ValidatorException;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Objects;
@@ -10,41 +12,15 @@ import java.util.Objects;
  * Managed bean for handling Y coordinate value in JSF application.
  * Offers methods to get and set Y value, and a validator to ensure the Y value falls within a specified range.
  */
+@Data
+@NoArgsConstructor
 public class YCoordinateBean implements Serializable {
     private Double y = 0.0;
-
-    public Double getYBeanValue(){
-        return y;
-    }
-
-    public void setYBeanValue(Double y){
-        this.y = y;
-    }
 
     public void validateYBeanValue(Object o){
         if (o == null){
             FacesMessage message = new FacesMessage("Y value should be in (-3, 5) interval");
             throw new ValidatorException(message);
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        YCoordinateBean yBean = (YCoordinateBean) o;
-        return Objects.equals(y, yBean.y);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(y);
-    }
-
-    @Override
-    public String toString() {
-        return "YCoordinateBean{" +
-                "yValue=" + y +
-                '}';
     }
 }
