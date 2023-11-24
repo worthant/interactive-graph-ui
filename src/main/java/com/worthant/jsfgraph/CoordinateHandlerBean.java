6aea75668333ac54bb05a9ed4795b98ada216907
worthant <b_dvorkin@niuitmo.ru>
2023-11-24 23:46:56 +0300

    :hammer: Apply Lombok, write javadoc

--- src/main/java/com/worthant/jsfgraph/CoordinateHandlerBean.java
+++ src/main/java/com/worthant/jsfgraph/CoordinateHandlerBean.java
@@ -1,38 +1,17 @@
 package com.worthant.jsfgraph;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 /**
  * Managed bean for handling coordinate inputs in JSF application.
  */
+@Data
+@NoArgsConstructor
 public class CoordinateHandlerBean implements Serializable {
-
     private double x;
     private double y;
-
     private double r;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public double getR() {
-        return r;
-    }
-
-    public void setR(double r) {
-        this.r = r;
-    }
 }
