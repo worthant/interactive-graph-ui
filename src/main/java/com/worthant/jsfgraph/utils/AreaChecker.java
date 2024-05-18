afe1590525afafc54f24ae01de8f412757880af5
worthant <b_dvorkin@niuitmo.ru>
2024-05-18 17:56:33 +0300

    :bug::test_tube: fix: Fix AreaChecker function & new tests

--- src/main/java/com/worthant/jsfgraph/utils/AreaChecker.java
+++ src/main/java/com/worthant/jsfgraph/utils/AreaChecker.java
@@ -6,29 +6,28 @@ package com.worthant.jsfgraph.utils;
 public class AreaChecker {
 
     /**
-     * Checks if a given point (x, y) lies within the blue area defined by
-     * radius r.
+     * Checks if a given point (x, y) lies within the defined area by radius r.
      *
      * @param x the x-coordinate of the point
      * @param y the y-coordinate of the point
      * @param r the radius defining the area
-     * @return true if the point is within the blue area, false otherwise
+     * @return true if the point is within the area, false otherwise
      */
     public static boolean isInArea(double x, double y, double r) {
         if (r == 0) {
             return false;
         }
-        // Check for the rectangle in the lower-left quadrant
-        if (x <= 0 && y <= 0) {
-            return x >= -r && y >= -r / 2;
+        // Check for the rectangle in the top-right quadrant
+        if (x >= 0 && y >= 0) {
+            return x <= r && y <= r / 2;
         }
-        // Check for the semicircle in the upper-right quadrant
-        else if (x >= 0 && y >= 0) {
-            return (x * x + y * y) <= (r * r) / 4 && x <= r;
+        // Check for the semicircle in the bottom-left quadrant
+        else if (x <= 0 && y <= 0) {
+            return (x * x + y * y) <= (r * r) / 4;
         }
-        // Check for the triangle in the lower-right quadrant
+        // Check for the triangle in the bottom-right quadrant
         else if (x >= 0 && y <= 0) {
-            return y >= (x - r / 2);
+            return y >= (2*x - r);
         }
         return false;
     }
