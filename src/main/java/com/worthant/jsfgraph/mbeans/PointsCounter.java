ac3bce71dfa327680de3154711360d1dbe98a593
worthant <b_dvorkin@niuitmo.ru>
2024-05-18 17:54:34 +0300

    :hammer: feat(mbean): Notifications && DAO sync

--- src/main/java/com/worthant/jsfgraph/mbeans/PointsCounter.java
+++ src/main/java/com/worthant/jsfgraph/mbeans/PointsCounter.java
@@ -3,11 +3,41 @@ package com.worthant.jsfgraph.mbeans;
 import javax.management.Notification;
 import javax.management.NotificationBroadcasterSupport;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Collection;
+
+import com.worthant.jsfgraph.db.ResultDAO;
+import com.worthant.jsfgraph.entity.ResultEntity;
 
 public class PointsCounter extends NotificationBroadcasterSupport implements PointsCounterMBean {
     private AtomicInteger totalPoints = new AtomicInteger(0);
     private AtomicInteger missedPoints = new AtomicInteger(0);
     private int consecutiveMisses = 0;
+    private long sequenceNumber = 1;
+    private ResultDAO resultDAO;
+
+    public PointsCounter(ResultDAO resultDAO) {
+        this.resultDAO = resultDAO;
+        initializeCounts();
+    }
+
+    private void initializeCounts() {
+        totalPoints.set(0);
+        missedPoints.set(0);
+        Collection<ResultEntity> results = resultDAO.getAllResults();
+        for (ResultEntity result : results) {
+            totalPoints.incrementAndGet();
+            if (!result.isResult()) {
+                missedPoints.incrementAndGet();
+            }
+        }
+    }
+
+    public void resetAndInitializeCounts() {
+        totalPoints.set(0);
+        missedPoints.set(0);
+        consecutiveMisses = 0;
+        initializeCounts();
+    }
 
     @Override
     public int getTotalPoints() {
@@ -29,7 +59,9 @@ public class PointsCounter extends NotificationBroadcasterSupport implements Poi
         missedPoints.incrementAndGet();
         consecutiveMisses++;
         if (consecutiveMisses == 2) {
-            sendNotification(new Notification("ConsecutiveMisses", this, 0));
+            Notification n = new Notification("ConsecutiveMisses", this, sequenceNumber++,
+                    System.currentTimeMillis(), "Two consecutive misses occurred");
+            sendNotification(n);
             consecutiveMisses = 0;
         }
     }
--- src/main/java/com/worthant/jsfgraph/mbeans/PointsCounterMBean.java
+++ src/main/java/com/worthant/jsfgraph/mbeans/PointsCounterMBean.java
@@ -6,4 +6,5 @@ public interface PointsCounterMBean {
     void incrementTotalPoints();
     void incrementMissedPoints();
     void resetConsecutiveMisses();
+    void resetAndInitializeCounts();
 }
